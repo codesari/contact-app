5ee24c36504d4569786ec5e576bd59140a8e0e86
codesari <ahmtsr81@gmail.com>
2022-11-20 09:17:02 +0300

    custom hook

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -10,7 +10,9 @@
 // }
 //? yukaridaki yapiyi fonksiyonel hale getirdik.
 
-import { getDatabase, push, ref, set } from "firebase/database";
+//* CRUD --> C
+import { getDatabase, push, ref, set, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
 import startFirebase from "./firebase";
 
 export const writeUserData = (user) => {
@@ -23,4 +25,29 @@ export const writeUserData = (user) => {
     gender: user.gender,
   });
   console.log("process successfully..");
+};
+
+//* CRUD --> R
+
+//? useFetch() bir custom hook
+export const useFetch = () => {
+  const [contact, setContact] = useState("");
+  const [isLoading, setIsLoading] = useState("");
+
+  useEffect(() => {
+    const db = getDatabase(startFirebase);
+    const userRef = ref(db, "users/");
+
+    onValue(userRef, (snapshot) => {
+      const data = snapshot.val();
+      const userArray = [];
+
+      for (let id in data) {
+        userArray.push({ id, ...data[id] });
+      }
+      setContact(userArray);
+      setIsLoading(false);
+    });
+    return { isLoading, contact };
+  }, []);
 };
